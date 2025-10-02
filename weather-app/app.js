97a2798d0bf5d7da79b24a8db4c120f37941c046
JordanMobleyCSD <jsinclair.mo@gmail.com>
2025-10-02 09:55:08 -0400

    Styling for UI

--- weather-app/app.js
+++ weather-app/app.js
@@ -31,37 +31,3 @@ console.log(process.argv)
 
 
 
-
-
-
-
-
-//
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
-
-
-
-
-
-
-// --- Old request --- //
-// const url = 'https://api.weatherstack.com/current?access_key=fabe066a5798f70ffa9ba43ffeffc112&query=35.0074,80.9451&units=f'
-// request({ url: url, json: true }, (error, response) => {
-//     if (error) {
-//         console.log('Unable to connect to weather service!')
-//     } else if (response.body.error) {
-//         console.log('Unable to find location')
-//     } else {
-//         console.log(response.body.current.weather_descriptions[0] + '- It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' +response.body.current.feelslike + ' degrees out.'
-//     )}
-    
-//     // console.log(response.body.current)
-    
-// })
--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -23,23 +23,3 @@ const geocode = (address, callback) => {
 
 
 module.exports = geocode
-
-
-
-
-// // Geocoding to change location into lat and long
-// // Input address and convert to lat/long 
-
-// const geoCodeURL = 'https://api.mapbox.com/search/geocode/v6/forward?q=Los%20Angeles&access_token=pk.eyJ1IjoiYWthcmk0IiwiYSI6ImNtZXgxMXZ1bjEwOXgya3BvbzNhYXZ4amkifQ.0AEHIBqpB6kwBLN2CgwP_g'
-
-// request({ url: geoCodeURL, json: true }, (error, response) => {
-// if (error) {
-//     console.log.log('Unable to connect to location services')
-// } else if (body.features.length === 0) {
-// console.log('Unable to find location. Try another search.')
-// } else {
-//     const latitude = body.features[0].geometry.coordinates[1]
-//     const longitude = body.features[0].geometry.coordinates[0]
-//     console.log(latitude, longitude)
-// }
-// })
